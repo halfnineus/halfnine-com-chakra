304f4b2561da0e5e8776f60695548a3edd873ec6
-_- <42155753+ochoadan@users.noreply.github.com>
2022-07-07 15:12:20 -0400

    Create index.tsx

---
+++ pages/blog/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head"
+const Index = () => {
+    return (
+        <>
+            <Head>
+                <title>Blog - International Product Development, Design and Production - Ochoa</title>
+                <meta name="description" content="" />
+                {/* <meta name="description" content="Read the latest news." /> */}
+            </Head>
+        </>
+    )
+}
+export default Index
