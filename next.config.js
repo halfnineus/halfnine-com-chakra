99266b4cecc134473150d94fb268e779a98ef8bb
-_- <42155753+ochoadan@users.noreply.github.com>
2022-07-09 11:30:17 -0500

    Stable-NO-Locales

--- next.config.js
+++ next.config.js
@@ -3,26 +3,41 @@ const nextConfig = {
 }
 
 module.exports = {
-  async redirects() {
-    return [
-       {
-        source: '/services',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/services/development',
-        destination: '/',
-        permanent: true,
-      },
-      // {
-      //   source: '/services/manufacturing',
-      //   destination: '/',
-      //   permanent: true,
-      // },
-    ];
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/services',
+  //       destination: '/',
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: '/services/development',
+  //       destination: '/',
+  //       permanent: true,
+  //     },
+  //     // {
+  //     //   source: '/services/manufacturing',
+  //     //   destination: '/',
+  //     //   permanent: true,
+  //     // },
+  //   ];
+  // },
+  reactStrictMode: true,
+  i18n: {
+    // providing the locales supported the application
+    locales: ["en", "es"],
+    //  default locale used when the non-locale paths are visited
+    defaultLocale: "en",
   },
-  nextConfig,
+
+  // i18n: {
+  //   // providing the locales supported by your application
+  //   locales: ["en", "es", "zh-CN", "zh-CN", "ru", "fr", "id", "de", "pt", "it", "ja", "hi", "ar","bn"],
+  //   //  default locale used when the non-locale paths are visited
+  //   defaultLocale: "en-US",
+  // },
+
+  // nextConfig,
   experimental: {
     outputStandalone: true,
   }
