bd3491eef64cfd9230c918dc0bf59857ac75c17f
-_- <42155753+ochoadan@users.noreply.github.com>
2023-02-23 13:36:30 -0500

    --

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-// Industrial IoT and Control Systems Automation Development and Consulting services
+// Industrial Automation Development and Consulting services
 
 import React from 'react'
 import { useRouter } from "next/router"
@@ -17,6 +17,7 @@ import {
   StackDivider,
   Center,
   keyframes,
+  HStack,
 } from '@chakra-ui/react'
 
 import indexdat from '../assets/index.json'
@@ -32,6 +33,8 @@ import { AiOutlineSolution } from 'react-icons/ai';
 import { BiBrain } from 'react-icons/bi';
 import { FaRegHandshake } from 'react-icons/fa';
 import { GiLeapfrog } from 'react-icons/gi';
+import { AiOutlineSetting } from 'react-icons/ai';
+import { MdOutlineContactSupport } from 'react-icons/md';
 
 interface FeatureProps1 {
   title: string;
@@ -95,7 +98,7 @@ interface FeatureiProps {
 export const Featurei = ({ title, children, icon }: FeatureiProps) => {
   return (
     <Box>
-      <Box color={"blue.600"} fontSize="2.5rem">
+      <Box color={"blue.600"} fontSize="2.8rem">
         {icon}
       </Box>
       <Stack mt="6">
@@ -155,9 +158,9 @@ const IndexPage = () => {
                 }
               ]}
             />
-            <Box as="section" pb={{ base: "none", lg: "12" }} pos="relative" px={{ base: '4', xl: '10' }}>
+            <Box as="section" pb={{ base: "none", lg: "12" }} pos="relative">
               <Box maxW={"100%"} mx="auto">
-                <Box maxW={{ md: 'full', lg: 'xl', xl: '3xl' }} pt={{ base: '6', lg: '8' }} pb={{ base: '16', lg: '40' }}>
+                <Box maxW={{ md: 'full', lg: 'xl', xl: '2xl' }} pt={{ base: '6', lg: '8' }} pb={{ base: '16', lg: '40' }}>
                   <Heading as="h1" size="3xl" lineHeight="1" fontWeight="extrabold" letterSpacing="tight">
                     {indexData.block1.h1_1}{' '}
                     <Box as="mark" color={'blue.600'} bg="transparent">{indexData.block1.h1_2}</Box>
@@ -165,11 +168,18 @@ const IndexPage = () => {
                   <Text maxW={{ lg: 'md', xl: '3xl' }} mt={4} fontSize="xl" fontWeight="medium" color={'gray.600'}>
                     {indexData.block1.txt_1}
                   </Text>
-                  <Link href={"/contact"}>
-                    <Button mt={6} size="md" colorScheme={'blue'} shadow="base" fontSize="md">
-                      {indexData.block1.button}
-                    </Button>
-                  </Link>
+                  <HStack spacing={6}>
+                    <Link href={"/contact"} target={'_blank'}>
+                      <Button mt={6} size="md" variant={'outline'} colorScheme={'blue'} shadow="base" fontSize="md" leftIcon={<AiOutlineSetting size={20} />}>
+                        {"The Process"}
+                      </Button>
+                    </Link>
+                    <Link href={"/contact"}>
+                      <Button mt={6} size="md" colorScheme={'blue'} shadow="base" fontSize="md" leftIcon={<MdOutlineContactSupport size={20} />}>
+                        {indexData.block1.button}
+                      </Button>
+                    </Link>
+                  </HStack>
                 </Box>
               </Box>
               <Box
@@ -178,53 +188,62 @@ const IndexPage = () => {
                 insetEnd={{ lg: '0' }}
                 bg={"gray.50"}
                 w={{ base: 'full', lg: '50%', xl: '42%' }}
-                height={{ base: '96', lg: 'full' }}
-                sx={{ clipPath: { lg: 'polygon(8% 0%, 100% 0%, 100% 100%, 0% 100%)' }, }}>
-                <Image userSelect={'none'} pointerEvents={'none'} height="100%" width="100%" objectFit="cover" src={'/img/index/x.jpg'} alt="Base" />
+              // height={{ base: '96', lg: 'full' }}
+              // sx={{ clipPath: { lg: 'polygon(8% 0%, 100% 0%, 100% 100%, 0% 100%)' }, }}
+              >
+                <Image
+                  userSelect={'none'}
+                  pointerEvents={'none'}
+                  // height="100%"
+                  // width="100%"
+                  // objectFit="cover"
+                  src={'/img/index/x.png'}
+                  alt="Base"
+                />
               </Box>
             </Box>
 
-            <Box minH={'5rem'} />
+            <Box minH={'8rem'} />
 
-            <Box pb={2} px={{ base: '4', xl: '10' }} mx="auto">
-              <Box w={{ base: "full", md: 11 / 12, xl: "full", }} mx="auto" textAlign={{ base: "left", md: "center", }}>
-                <Heading
-                  mb={2}
-                  fontSize={{ base: "4xl", md: "6xl", }}
-                  fontWeight="bold"
-                  lineHeight="none"
-                  letterSpacing={{ base: "normal", md: "tight", }}
+            {/* <Box pb={2} px={{ base: '4', xl: '10' }} mx="auto"> */}
+            <Box w={{ base: "full", md: 11 / 12, xl: "full", }} mx="auto" textAlign={{ base: "left", md: "center", }}>
+              <Heading
+                mb={2}
+                fontSize={{ base: "4xl", md: "5xl" }}
+                fontWeight="bold"
+                lineHeight="none"
+                letterSpacing={{ base: "normal", md: "tight", }}
+              >
+                {indexData.block2.h2_1}{" "}
+                <Text
+                  bgClip="text"
+                  display={'inline'}
+                  pr={{ md: "none", lg: 1 }}
+                  pb={{ base: 2, md: 3, lg: "none" }}
+                  animation={`${bounce} 5s ease infinite`}
+                  bgImage={"linear-gradient(to right, #2b6cb0 32%, transparent)"}
+                  fontWeight="extrabold"
                 >
-                  {indexData.block2.h2_1}{" "}
-                  <Text
-                    bgClip="text"
-                    display={'inline'}
-                    pr={{ md: "none", lg: 1 }}
-                    pb={{ base: 2, md: 3, lg: "none" }}
-                    animation={`${bounce} 5s ease infinite`}
-                    bgImage={"linear-gradient(to right, #2b6cb0 32%, transparent)"}
-                    fontWeight="extrabold"
+                  {indexData.block2.h2_2}
+                </Text>{" "}
+              </Heading>
+              <Text px={{ base: 0, lg: 24, }} mb={4} fontSize={{ base: "lg", md: "xl", }} color={"gray.700"}>
+                {indexData.block2.txt}{" "}
+                <Link href={"/services"}>
+                  <Button
+                    userSelect={'text'}
+                    fontSize="xl"
+                    fontWeight="medium"
+                    color={'blue.400'}
+                    variant={'link'}
                   >
-                    {indexData.block2.h2_2}
-                  </Text>{" "}
-                </Heading>
-                <Text px={{ base: 0, lg: 24, }} mb={4} fontSize={{ base: "lg", md: "xl", }} color={"gray.700"}>
-                  {indexData.block2.txt}{" "}
-                  <Link href={"/services"}>
-                    <Button
-                      userSelect={'text'}
-                      fontSize="xl"
-                      fontWeight="medium"
-                      color={'blue.400'}
-                      variant={'link'}
-                    >
-                      {indexData.block2.txturl}
-                    </Button>
-                  </Link>
-                  .
-                </Text>
-              </Box>
+                    {indexData.block2.txturl}
+                  </Button>
+                </Link>
+                .
+              </Text>
             </Box>
+            {/* </Box> */}
 
             <Center px={{ base: '4', lg: '0' }}>
               <Link href={'https://www.zedir.com/img/index/Basic-IoT-Enviroment.png'} target={'_blank'}>
@@ -242,9 +261,9 @@ const IndexPage = () => {
               </Link>
             </Center>
 
-            <Box minH={'5rem'} />
+            <Box minH={'8rem'} />
 
-            <Box px={{ base: '4', xl: '10' }}>
+            <Box>
               <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
                 <Feature1
                   icon={<Icon as={FcServices} w={10} h={10} />}
@@ -266,77 +285,74 @@ const IndexPage = () => {
               </SimpleGrid>
             </Box >
 
-            <Box minH={'5rem'} />
+            <Box minH={'8rem'} />
 
-            <Container px={{ base: '4', xl: '10' }} maxW={'container.xl'}>
-              <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
-                <Stack spacing={4}>
-                  <Heading pl={{ xl: 2, md: 0 }} pt={8}>{indexData.block4.title}</Heading>
-                  <Text pl={{ xl: 2, md: 0 }} color={'gray.500'} fontSize={'lg'}>
-                    {indexData.block4.description}
-                  </Text>
-                  <Stack
-                    spacing={2}
-                    divider={<StackDivider borderColor={'gray.100'} />}>
-                    <Feature2
-                      refx={'/services/development'}
-                      icon={<Icon as={IoPeopleOutline} color={'blue.600'} w={5} h={5} />}
-                      iconBg={'blue.100'}
-                      text={indexData.block4.srvc1}
-                    />
-                    <Feature2
-                      refx={'/services/digitalization'}
-                      icon={<Icon as={IoRocketOutline} color={'green.600'} w={5} h={5} />}
-                      iconBg={'green.100'}
-                      text={indexData.block4.srvc2}
-                    />
-                    <Feature2
-                      refx={'/services/consultation'}
-                      icon={<Icon as={IoBulbOutline} color={'orange.600'} w={5} h={5} />}
-                      iconBg={'orange.100'}
-                      text={indexData.block4.srvc3}
-                    />
-                  </Stack>
-                </Stack>
-                <Flex>
-                  <Image
-                    rounded={"2xl"}
-                    border='1px'
-                    borderColor={'gray.600'}
-                    alt={'feature image'}
-                    src={'/img/index/iot.jpg'}
-                    objectFit={'cover'}
-                    userSelect={'none'}
-                    pointerEvents={'none'}
+            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+              <Stack spacing={4}>
+                <Heading pl={{ xl: 2, md: 0 }} pt={8}>{indexData.block4.title}</Heading>
+                <Text pl={{ xl: 2, md: 0 }} color={'gray.500'} fontSize={'lg'}>
+                  {indexData.block4.description}
+                </Text>
+                <Stack
+                  spacing={2}
+                  divider={<StackDivider borderColor={'gray.100'} />}>
+                  <Feature2
+                    refx={'/services/development'}
+                    icon={<Icon as={IoPeopleOutline} color={'blue.600'} w={5} h={5} />}
+                    iconBg={'blue.100'}
+                    text={indexData.block4.srvc1}
                   />
-                </Flex>
+                  <Feature2
+                    refx={'/services/digitalization'}
+                    icon={<Icon as={IoRocketOutline} color={'green.600'} w={5} h={5} />}
+                    iconBg={'green.100'}
+                    text={indexData.block4.srvc2}
+                  />
+                  <Feature2
+                    refx={'/services/consultation'}
+                    icon={<Icon as={IoBulbOutline} color={'orange.600'} w={5} h={5} />}
+                    iconBg={'orange.100'}
+                    text={indexData.block4.srvc3}
+                  />
+                </Stack>
+              </Stack>
+              <Flex>
+                <Image
+                  rounded={"2xl"}
+                  border='1px'
+                  borderColor={'gray.600'}
+                  alt={'feature image'}
+                  src={'/img/index/iot.jpg'}
+                  objectFit={'cover'}
+                  userSelect={'none'}
+                  pointerEvents={'none'}
+                />
+              </Flex>
+            </SimpleGrid>
+
+            <Box minH={'8rem'} />
+
+            <Box mx="auto">
+              <SimpleGrid
+                columns={{ base: 1, md: 2, lg: 4 }}
+                spacing={{ base: '12', md: '8', lg: '2' }}
+              >
+                <Featurei icon={<FaRegHandshake />} title={indexData.features.f1t}>
+                  {indexData.features.f1d}
+                </Featurei>
+                <Featurei icon={<BiBrain />} title={indexData.features.f2t}>
+                  {indexData.features.f2d}
+                </Featurei>
+                <Featurei icon={<GiLeapfrog />} title={indexData.features.f3t}>
+                  {indexData.features.f3d}
+                </Featurei>
+                <Featurei icon={<AiOutlineSolution />} title={indexData.features.f4t}>
+                  {indexData.features.f4d}
+                </Featurei>
               </SimpleGrid>
-            </Container>
-
-            <Box minH={'5rem'} />
-
-            <Box px={{ base: '4', xl: '10' }}>
-              <Box mx="auto">
-                <SimpleGrid
-                  columns={{ base: 1, md: 2, lg: 4 }}
-                  spacing={{ base: '12', md: '8', lg: '2' }}
-                >
-                  <Featurei icon={<FaRegHandshake />} title={indexData.features.f1t}>
-                    {indexData.features.f1d}
-                  </Featurei>
-                  <Featurei icon={<BiBrain />} title={indexData.features.f2t}>
-                    {indexData.features.f2d}
-                  </Featurei>
-                  <Featurei icon={<GiLeapfrog />} title={indexData.features.f3t}>
-                    {indexData.features.f3d}
-                  </Featurei>
-                  <Featurei icon={<AiOutlineSolution />} title={indexData.features.f4t}>
-                    {indexData.features.f4d}
-                  </Featurei>
-                </SimpleGrid>
-              </Box >
             </Box >
-            <Box minH={'5rem'} />
+
+            <Box minH={'8rem'} />
 
           </>
         )
