10aa6404433429f70a3521985cc1874041137300
Your Name <42155753+ochoadan@users.noreply.github.com>
2023-05-09 08:45:54 -0400

    oldbox

--- components/fadeInBox.tsx
+++ components/fadeInBox.tsx
@@ -1,4 +1,4 @@
-import { Box, BoxProps, useMediaQuery } from '@chakra-ui/react';
+import { Box, BoxProps } from '@chakra-ui/react';
 import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import React from 'react'
@@ -8,7 +8,6 @@ type FadeInBoxProps = BoxProps & {
   time?: number;
   distance?: number;
   delay?: number;
-  disableBelowBreakpoint?: "base" | "sm" | "md" | "lg" | "xl" | "2xl";
 };
 
 const MotionBox = motion(Box);
@@ -19,63 +18,43 @@ const FadeInBox: React.FC<FadeInBoxProps> = ({
   time = 0.5,
   distance = 50,
   delay = 0,
-  disableBelowBreakpoint = "md",
   ...props
 }) => {
   const controls = useAnimation();
   const [ref, inView] = useInView();
 
-  const [isBase] = useMediaQuery("(max-width: 30em)");
-  const [isSm] = useMediaQuery("(min-width: 30em) and (max-width: 48em)");
-  const [isMd] = useMediaQuery("(min-width: 48em) and (max-width: 62em)");
-
-  const shouldAnimate = React.useMemo(() => {
-    if (!disableBelowBreakpoint) return true;
-    if (disableBelowBreakpoint === "base" && isBase) return false;
-    if (disableBelowBreakpoint === "sm" && (isBase || isSm)) return false;
-    if (disableBelowBreakpoint === "md" && (isBase || isSm || isMd)) return false;
-    return true;
-  }, [disableBelowBreakpoint, isBase, isSm, isMd]);
-
   React.useEffect(() => {
-    if (inView && shouldAnimate) {
+    if (inView) {
       controls.start({
         opacity: 1,
         x: 0,
         y: 0,
-        transition: {
-          duration: time,
+        transition: { 
+          duration: time, 
           delay,
           ease: "easeOut"
         },
       });
     }
-    if (!shouldAnimate) {
-      controls.set({
-        opacity: 1,
-        x: 0,
-        y: 0
-      });
-    }
-  }, [controls, inView, shouldAnimate]);
+  }, [controls, inView]);
 
   return (
     <MotionBox
       ref={ref}
       initial={{
-        opacity: shouldAnimate ? 0 : 1,
+        opacity: 0,
         x:
           fadeDirection === 'left'
             ? -distance
             : fadeDirection === 'right'
-              ? distance
-              : 0,
+            ? distance
+            : 0,
         y:
           fadeDirection === 'up'
             ? -distance
             : fadeDirection === 'down'
-              ? distance
-              : 0,
+            ? distance
+            : 0,
       }}
       animate={controls}
       {...props}
